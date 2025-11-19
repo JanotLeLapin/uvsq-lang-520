20b4790b02ef7fa1f1d550f779930d2dde9a5cc1
Joseph DALY <joseph300905@gmail.com>
2025-11-19 20:37:30 +0100

    add dev tools to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,12 @@
 { gcc
 , flex
 , python3
+, clang-tools
+, valgrind
 , mkShell
 }: mkShell {
-  buildInputs = [ gcc flex python3 ];
+  buildInputs = [
+    gcc flex python3
+    clang-tools valgrind
+  ];
 }
