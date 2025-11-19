ef8b4f757381fb531a19fa144c7622fa134703ec
Joseph DALY <joseph300905@gmail.com>
2025-11-19 14:16:22 +0100

    définition des tokens

---
+++ tokens.h
@@ -0,0 +1,12 @@
+// Définition des tokens
+#define CONST_A 0x11 /* a */
+#define CONST_B 0x12 /* b */
+#define CONST_C 0x13 /* c */
+#define CONST_E 0x14 /* epsilon */
+
+#define OP_ADD  0x21 /* + */
+#define OP_ANY  0x22 /* * */
+#define OP_CAT  0x23 /* . */
+
+#define PAR_O   0x31 /* ( */
+#define PAR_C   0x32 /* ) */
